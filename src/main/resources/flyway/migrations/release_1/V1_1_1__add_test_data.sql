167da6d5a3729b52875651971a1a3bcc52b8ca36
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-07-13 17:07:40 +0200

    Add script with data insertion

---
+++ src/main/resources/flyway/migrations/release_1/V1_1_1__add_test_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.flyway_test ("key",value) VALUES ('1','test -- 1') ;
+INSERT INTO public.flyway_test ("key",value) VALUES ('2','test -- 2') ;
+INSERT INTO public.flyway_test ("key",value) VALUES ('3','test -- 3') ;

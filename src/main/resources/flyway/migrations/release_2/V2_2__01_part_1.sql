397e12ae680bf3002a9f20b040a337a87578f494
Guillaume Husta <g.husta@free.fr>
2018-07-25 18:46:38 +0200

    Add multiple migration files per version

---
+++ src/main/resources/flyway/migrations/release_2/V2_2__01_part_1.sql
@@ -0,0 +1,6 @@
+-- create table (DDL)
+CREATE TABLE table_22(
+  id INTEGER,
+  value VARCHAR(255),
+  PRIMARY KEY(id)
+);
---
+++ src/main/resources/flyway/migrations/release_2/V2_2__02_part_2.sql
@@ -0,0 +1,3 @@
+-- insert some data (DML)
+INSERT INTO public.table_22 (id, value) VALUES (1, 'test -- 1') ;
+INSERT INTO public.table_22 (id, value) VALUES (2, 'test -- 2') ;
